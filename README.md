ab42a85f6f3412e4580084fa378a46f7de91e46c
Nazarii <nazariikubik@gmail.com>
2021-03-22 11:46:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mad Libs Generator
+
+Input the following to and see what happens next.
---
+++ mad_libs.py
@@ -0,0 +1,20 @@
+noun1 = input("Enter a Noun: ")
+noun2 = input("Enter a Noun: ")
+ingverb = input("Enter a ing-verb: ")
+verb = input("Enter verb: ")
+plural_noun1 = input("Enter a Plural Noun 1: ")
+plural_noun2 = input("Enter a Plural Noun 2: ")
+plural_noun3 = input("Enter a Plural Noun 3: ")
+plural_noun4 = input("Enter a Plural Noun 4: ")
+
+
+print("When I go to the arcade with my" + plural_noun1 + "there are lots of games to play. I spend lots of time there with my friends. ")
+print("In the game X-Men you can be different" + plural_noun2)
+print("The point of the game is to" + verb + "every robot. You also need to save people. Then you can go to the next level. ")
+print("In the game Star Wars you are Luke Skywalker and you try to destroy every" + noun1 )
+print("In a car racing/motorcycle racing game you need to beat every computerized vehicle that you are" + ingverb + "against. ")
+print("There are a whole lot of other cool games. When you play some games you win" + plural_noun3 + " for certain scores. ")
+print("Once you're done you can cash in your tickets to get a big" + noun2 )
+print("You can save your" + plural_noun4 + "for another time. When I went to this arcade I didn't believe how much fun it would be. So far I have had a lot of fun every time I've been to this great arcade!  ")
+
+
